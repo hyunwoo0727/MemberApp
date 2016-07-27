d01d29ad12597a6e5e971dc48aa2ea45806ab791
2hwooo727@gmail.com <2hwooo727@gmail.com>
2016-07-27 12:35:11 +0900

    start

---
+++ app/src/main/java/com/example/hyunwoo0727/memberapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hyunwoo0727.memberapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
